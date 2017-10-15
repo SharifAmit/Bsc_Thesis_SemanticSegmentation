a1c93f35804f4845893d4f988b2718f40f4097af
Sharif Amit Kamran <SharifAmit@users.noreply.github.com>
2017-10-15 15:18:38 +0600

    Update demoplay.py

--- demoplay.py
+++ demoplay.py
@@ -26,42 +26,135 @@ palette = {(  0,   0,   0) : 0 ,
              (128, 192,   0) : 19,
              (  0,  64, 128) : 20 }
 # load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
+im = Image.open('2007_000480.jpg')
+in_ = np.array(im, dtype=np.float32)
+in_ = in_[:,:,::-1]
+in_ -= np.array((104.00698793,116.66876762,122.67891434))
+in_ = in_.transpose((2,0,1))
+
+# load net
+net = caffe.Net('deploy.prototxt', 'vgg19_FCN.caffemodel', caffe.TEST)
+# shape for input (data blob is N x C x H x W), set data
+net.blobs['data'].reshape(1, *in_.shape)
+net.blobs['data'].data[...] = in_
+# run net and take argmax for prediction
+net.forward()
+out = net.blobs['score'].data[0].argmax(axis=0)
+img_3d=np.zeros((out.shape[0],out.shape[1],3),dtype=np.uint8)
+for x in range(img_3d.shape[0]):
+    			for y in range(img_3d.shape[1]):
+				if out[x][y]==0:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=0
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=0
+				if out[x][y]==1:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=128
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=0
+				if out[x][y]==2:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=0
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=0
+				if out[x][y]==3:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=128
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=0
+				if out[x][y]==4:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=0
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=128
+				if out[x][y]==5:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=128
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=128
+				if out[x][y]==6:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=0
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=128
+				if out[x][y]==7:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=128
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=128
+				if out[x][y]==8:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=64
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=0
+				if out[x][y]==9:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=192
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=0
+				if out[x][y]==10:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=64
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=0			
+				if out[x][y]==11:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=192
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=0
+				if out[x][y]==12:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=64
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=128			
+				if out[x][y]==13:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=192
+						img_3d[x][y][1]=0
+						img_3d[x][y][2]=128
+							
+				if out[x][y]==14:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=64
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=128						
+				if out[x][y]==15:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=192
+						img_3d[x][y][1]=128
+						img_3d[x][y][2]=128
+				if out[x][y]==16:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=0
+						img_3d[x][y][1]=64
+						img_3d[x][y][2]=0
+				if out[x][y]==17:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=128
+						img_3d[x][y][1]=64
+						img_3d[x][y][2]=0			
+				if out[x][y]==18:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=0
+						img_3d[x][y][1]=192
+						img_3d[x][y][2]=0
+							
+				if out[x][y]==19:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=128
+						img_3d[x][y][1]=192
+						img_3d[x][y][2]=0						
+				if out[x][y]==20:
+					for z in range(img_3d.shape[2]):
+						img_3d[x][y][0]=0
+						img_3d[x][y][1]=64
+						img_3d[x][y][2]=128
+
+img = Image.fromarray(img_3d)
+img.save('ourmodelfinal.png')
 
 
 
-dataset = np.loadtxt('../test.txt', dtype=str)
-save_dir = '/home/sharif/Desktop/caffe-master/comp6_test_cls/'
-image_path = '/home/sharif/Desktop/caffe-master/data/VOC2012/JPEGImages/'
-for idx in dataset:
-	
-	im = Image.open(image_path+idx+'.jpg')
-	in_ = np.array(im, dtype=np.float32)
-	in_ = in_[:,:,::-1]
-	in_ -= np.array((104.00698793,116.66876762,122.67891434))
-	in_ = in_.transpose((2,0,1))
-	# load net
-	net = caffe.Net('deploy.prototxt', 'vocSBDall_iter_400000.caffemodel', caffe.TEST)
-	# shape for input (data blob is N x C x H x W), set data	
-	net.blobs['data'].reshape(1, *in_.shape)
-	net.blobs['data'].data[...] = in_
-	# run net and take argmax for prediction
-	net.forward()
-#out = net.blobs['score'].data[0].argmax(axis=0)
-#out = np.array(out, np.uint8)
-	im = Image.fromarray(net.blobs['score'].data[0].argmax(0).astype(np.uint8), mode='P')
-	im.save(os.path.join(save_dir, idx + '.png'))
-#img_3d=np.zeros((out.shape[0],out.shape[1],3),dtype=np.uint8)
-#for x in range(img_3d.shape[0]):
-#    			for y in range(img_3d.shape[1]):
-#				if out[x][y]==15:
-#					for z in range(img_3d.shape[2]):
-#						img_3d[x][y][0]=0
-#						img_3d[x][y][1]=64
-#						img_3d[x][y][2]=0
-#				if out[x][y]==1:
-#					for z in range(img_3d.shape[2]):
-#						img_3d[x][y][0]=128
-#						img_3d[x][y][1]=0
-#						img_3d[x][y][2]=0
-#img = Image.fromarray(img_3d)
-#img.save('test.png')
+
+
